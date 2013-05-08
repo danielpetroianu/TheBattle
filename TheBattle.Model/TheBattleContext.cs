5ea7a99384f4c76011facd3da4d0491e3911b57c
Petroianu Daniel <petroianudaniel@gmail.com>
2013-05-09 00:46:43 +0300

    created Soldier and Army model. Also added the DbContext

---
+++ TheBattle.Model/TheBattleContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace TheBattle.Model
+{
+    class TheBattleContext : DbContext
+    {
+        DbSet<ISoldier> Soldiers { get; set; }
+        DbSet<Army> Armies { get; set; }
+    }
+}
---
+++ TheBattle.Test/SoldierTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheBattle.Test
+{
+    [TestClass]
+    public class SoldierTest
+    {
+        // naming convention
+        //[TestMethod]
+        //public void NameOfTheMethod_actionToBeTested_expectedResult()
+        //{
+        //}
+    }
+}
