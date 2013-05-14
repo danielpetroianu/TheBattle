ccc3ce349777c7d4ac3e23d2521f94c1131c49fb
ancagusetoiu <anca_gusetoiu@yahoo.com>
2013-05-14 10:46:07 -0700

    Florian Parvan - added weapon model

---
+++ TheBattle.Model/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheBattle.Model
+{
+    public abstract class Weapon
+    {
+        public abstract int GetDamage();
+
+        public  isBetterThan(Weapon otherWeapon)
+        {
+            return this.GetDamage() >= otherWeapon.GetDamage();
+        }
+    }
+}
