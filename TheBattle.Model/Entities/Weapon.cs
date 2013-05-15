fd9762390d95138496c740ced94b774f0de8e1f9
Petroianu Daniel <petroianudaniel@gmail.com>
2013-05-15 22:58:56 +0300

    (re)added weapon model

---
+++ TheBattle.Model/Entities/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBattle.Model.Entities
+{
+    public class Weapon : DbBase.DbEntity<int>
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Damage { get; set; }
+
+        public bool IsBetterThan(Weapon otherWeapon)
+        {
+            return this.Damage >= otherWeapon.Damage;
+        }
+    }
+}
