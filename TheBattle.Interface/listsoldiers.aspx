9ba7e3c03e2ee0e5f9b53eb90c149380db9ed3b0
anca <anca_gusetoiu@yahoo.com>
2013-05-16 21:09:02 +0300

    other changes

--- TheBattle.Interface/listsoldiers.aspx
+++ TheBattle.Interface/listsoldiers.aspx
@@ -5,7 +5,20 @@
 <br/><br/>
 <a href="soldiers.aspx">Add soldier</a>
 <br/><br/>
+
+  <div class="item">
+        <h3>
+            Soldiers</h3><br />
+        <div class="item">
+            <div class="body">
     <asp:Repeater ID="soldiers" runat="server">
-    <ItemTemplate><div class=soldier><%# Eval("Name") %></div></ItemTemplate>
+    <ItemTemplate><div>
+        <div class=soldier><%# Eval("Name") %><br/><br/><br/><br/><br/><br/>
+        <a href = "soldiers.aspx?id=<%# Eval("Id") %>" class = 'edit'> Edit this soldier </a></div>
+    </ItemTemplate>
+    
     </asp:Repeater>
+    </div>
+    </div>
+    </div>
 </asp:Content>
--- TheBattle.Interface/soldiers.aspx.cs
+++ TheBattle.Interface/soldiers.aspx.cs
@@ -16,20 +16,57 @@ namespace TheBattle.Interface
         private WeaponRepository _weapon = new WeaponRepository();
         protected void Page_Load(object sender, EventArgs e)
         {
+        
 
             weapon_ddl.DataSource = _weapon.GetAll().ToList();
             weapon_ddl.DataTextField = "Name";
             weapon_ddl.DataValueField = "Id";
             weapon_ddl.DataBind();
+
+
+            int soldier_id;
+            try
+            {
+                soldier_id = Convert.ToInt32(Request.QueryString["id"]);
+                Soldier soldier = _repository.FindBy(s => s.Id == soldier_id).FirstOrDefault();
+                soldier_name.Text = soldier.Name;
+                weapon_ddl.SelectedIndex = Convert.ToInt32(soldier.Weapon);
+            }
+            catch (Exception ex)
+            { }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Soldier soldier = new Soldier(soldier_name.Text);
+           
+
+            int soldier_id = 0;
+            try
+            {
+                soldier_id = Convert.ToInt32(Request.QueryString["id"]);
+            }
+            catch(Exception ex)
+            {
+                soldier_id = 0;
+            }
+
+            Soldier soldier ;
             int weapon_id = Convert.ToInt32(weapon_ddl.SelectedValue);
-            Weapon w = _weapon.FindBy(weapon => weapon.Id == weapon_id).FirstOrDefault();
-            soldier.Weapon = w;
-            _repository.Add(soldier).Save();
+            if (soldier_id == 0)
+            {
+                soldier = new Soldier(soldier_name.Text);
+                Weapon w = _weapon.FindBy(weapon => weapon.Id == weapon_id).FirstOrDefault();
+                soldier.Weapon = w;
+                _repository.Add(soldier).Save();
+
+            }
+            else
+            {
+                soldier = _repository.FindBy(s => s.Id == soldier_id).FirstOrDefault();
+                Weapon w = _weapon.FindBy(weapon => weapon.Id == weapon_id).FirstOrDefault();
+                soldier.Weapon = w;
+                _repository.Update(soldier).Save(); 
+            }
             Response.Redirect("listsoldiers.aspx");
         }
     }
